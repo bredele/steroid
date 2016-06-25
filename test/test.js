97ff393021617e6eb47612c73ed37452edc8249a
bredele <olivier.wietrich@gmail.com>
2016-06-25 10:05:59 -0600

    should join array into substitution

--- test/test.js
+++ test/test.js
@@ -7,26 +7,33 @@ var through = require('through')
 var test = require('tape')
 
 
-test('should stream simple html element as string', (assert) => {
+test('should stream html element', (assert) => {
   assert.plan(1)
   stream`<button>hello world!</button>`
     .pipe(writer( result => assert.equal(result, '<button>hello world!</button>')))
 })
 
 
-test('should stream simple html element as string using interpolation', (assert) => {
+test('should substitute simple string', (assert) => {
   assert.plan(1)
   var name = 'olivier'
   stream`<button>hello ${name}!</button>`
     .pipe(writer( result => assert.equal(result, '<button>hello olivier!</button>')))
 });
 
-test('should stream multiple html elements', (assert) => {
+test('should substitute multiple strings', (assert) => {
   assert.plan(1)
   var name = 'olivier'
   var friend = 'klara'
   stream`<button>hello ${name} and <span>${friend}</span></button>`
     .pipe(writer(result => assert.equal(result, '<button>hello olivier and <span>klara</span></button>')))
+})
+
+test('should substitute array', (assert) => {
+  assert.plan(1)
+  var arr = ['olivier', 'klara']
+  stream`<button>hello ${arr}</button>`
+    .pipe(writer( result => assert.equal(result, '<button>hello olivier klara</button>')))
 })
 
 /**
