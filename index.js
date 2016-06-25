01c01785301ca70555faabe97714188f9b3c6d1e
bredele <olivier.wietrich@gmail.com>
2016-06-25 10:16:23 -0600

    join array in subsitution
    useful for class names

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ module.exports = function(arr, ...args) {
   var stream = new Read
   stream._read = function() {}
   arr.forEach((str, index) => {
-    var value = args[index] || ''
-    stream.push(str + value)
+    var value = args[index]
+    if(value instanceof Array) value = value.join(' ')
+    stream.push(str + (value || ''))
   })
   stream.push(null)
   return stream
