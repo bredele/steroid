fa957d271b64372904558a3c3846499eb0119916
bredele <olivier.wietrich@gmail.com>
2016-06-25 10:18:45 -0600

    added classes example

--- example/server.js
+++ example/server.js
@@ -12,7 +12,8 @@ var html = require('..')
 
 http.createServer((req, res) => {
   let name = 'olivier'
-  html`<button>
+  let classes = ['selected', 'button']
+  html`<button class='${classes}'>
     hello <b>${name}</b>
   </button>`.pipe(res)
 }).listen(8080)
