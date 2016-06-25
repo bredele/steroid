e982bf752a9fca8c89b1a80eab6007e3d408715f
bredele <olivier.wietrich@gmail.com>
2016-06-25 10:00:06 -0600

    added template string interpolation

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+/**
+ * Module dependencies.
+ */
 
+var Read = require('stream').Readable
+
+
+/**
+ * Tagged template transforming string into
+ * stream.
+ *
+ * @param {Array} arr
+ * @api public
+ */
+
+module.exports = function(arr, ...args) {
+  var stream = new Read
+  stream._read = function() {}
+  arr.forEach((str, index) => {
+    var value = args[index] || '';
+    stream.push(str + value)
+  })
+  stream.push(null)
+  return stream
+};
--- test/test.js
+++ test/test.js
@@ -2,26 +2,34 @@
  * Test dependencies.
  */
 
-var stream = require('..');
-var through = require('through');
-var test = require('tape');
+var stream = require('..')
+var through = require('through')
+var test = require('tape')
 
 
 test('should stream simple html element as string', (assert) => {
+  assert.plan(1)
   stream`<button>hello world!</button>`
-    .pipe(writer( result => assert.equal(result, '<button>hello world!</button>')));
-});
+    .pipe(writer( result => assert.equal(result, '<button>hello world!</button>')))
+})
 
+
+test('should stream simple html element as string using interpolation', (assert) => {
+  assert.plan(1);
+  var name = 'olivier';
+  stream`<button>hello ${name}!</button>`
+    .pipe(writer( result => assert.equal(result, '<button>hello olivier!</button>')))
+})
 
 /**
  * Create writable stream.
  */
 
 function writer(cb) {
-  var result = '';
+  var result = ''
   return through( chunk => {
-    result += chunk;
+    result += chunk
   }, () => {
-    cb(result);
+    cb(result)
   })
 }
